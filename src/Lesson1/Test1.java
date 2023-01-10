3527b59c679bcac07004ee7e6071095bce12b2a4
Ivangolovinn <go-lo-vin@ya.ru>
2023-01-10 11:47:35 +0300

    w

--- src/Lesson1/Test1.java
+++ src/Lesson1/Test1.java
@@ -8,7 +8,7 @@ public class Test1 {
         System.out.println("Много лет размышлял я над жизнью земной.");
         System.out.println("Непонятного нет для меня под луной.");
         System.out.println("Мне известно, что мне ничего не известно!");
-        System.out.println("Вот последняя правда, открытая мнjой.");
+        System.out.println("Вот последняя правда, открытая мной.");
         System.out.println("                                О.Хайям");                       
         
         
